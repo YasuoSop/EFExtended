6841137e8eeb48da5160ec4d1fef68592a55997a
3137854779 <3137854779@qq.com>
2018-03-04 21:33:53 +0800

    First Commit

---
+++ EFExtendedServices/EFExtendedService.cs
@@ -0,0 +1,14 @@
+﻿using EFExtended.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFramework.Extensions;
+
+namespace EFExtended.Services
+{
+    public class EFExtendedService
+    {
+    }
+}
